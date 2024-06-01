bf2c23f70fd7efb6e014cbe29b1d57e8b1de532b
Caspita <lucascaspani@gmail.com>
2024-06-01 20:41:14 -0300

    Cambio de API

--- js/conexionAPI.js
+++ js/conexionAPI.js
@@ -1,12 +1,12 @@
 
 async function listarProductos(){
-    const conexion = await fetch("https://rest-api-fake.vercel.app/productos")
+    const conexion = await fetch("https://api-fake-nine.vercel.app/productos")
     const conexionConvertida = await conexion.json()
     return conexionConvertida
 }
 
 async function enviarProducto(titulo, precio, imagen){
-    const conexion = await fetch("https://rest-api-fake.vercel.app/productos", {
+    const conexion = await fetch("https://api-fake-nine.vercel.app/productos", {
         method: "POST",
         headers: {"Content-type":"application/json"},
         body: JSON.stringify({
@@ -19,7 +19,7 @@ async function enviarProducto(titulo, precio, imagen){
     return conexionConvertida;
 }
 async function eliminarProducto(id){
-    return fetch(`https://rest-api-fake.vercel.app/productos/${id}`,{
+    return fetch(`https://api-fake-nine.vercel.app/productos/${id}`,{
         method: "DELETE"
     });
 }
